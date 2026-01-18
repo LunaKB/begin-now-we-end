3dc0a0fbce9674a43a862f9b2da70918b9aab2d2
Chaz-Rae Moncrieffe <cmoncr4@wgu.edu>
2026-01-18 16:20:35 -0700

    Initial commit of project.

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.cmoncrieffe"
+version = "0.0.1-SNAPSHOT"
+description = "begin-now-we-end"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("tools.jackson.module:jackson-module-kotlin")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(fileTree("lib") { include("*.jar") })
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/cmoncrieffe/gundalisgame/BeginNowWeEndApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cmoncrieffe.gundalisgame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeginNowWeEndApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
